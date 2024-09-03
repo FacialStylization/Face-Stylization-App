cb7b2f59e87de7a687ebf58c68043c2a2ee21e95
neverbiasu <1751162157@qq.com>
2024-09-03 21:00:57 +0800

    Update GenerateView

--- src/views/GenerateView.vue
+++ src/views/GenerateView.vue
@@ -14,20 +14,21 @@ const description = ref(
   <div
     class="dark:bg-gray-900 dark:text-gray-100 flex flex-col items-center justify-center w-full min-h-screen"
   >
+    <h1 class="text-6xl font-serif mb-8">T2Vtuber</h1>
     <!-- Image Container, fills the screen width -->
     <div
-      class="bg-white dark:bg-gray-800 rounded-2xl p-4 max-w-screen-2xl w-full mx-auto shadow-md flex flex-col justify-between h-auto"
+      class="bg-gray-200 dark:bg-gray-800 rounded-2xl p-4 max-w-screen-2xl w-full mx-auto shadow-md flex flex-col justify-between h-auto"
     >
       <!-- Generated Label -->
       <div
         class="text-left font-serif text-2xl text-black dark:text-white rounded-2xl px-4 py-2 mb-4 inline-block"
       >
-        generated
+        <h2 class="text-4xl">generated</h2>
       </div>
 
       <!-- Image Display, centered in container -->
       <div
-        class="relative flex-grow flex items-center justify-center rounded-2xl border border-gray-200 dark:border-gray-800 p-4"
+        class="relative flex-grow flex items-center justify-center rounded-2xl dark:border-gray-800 p-4"
       >
         <img :src="imageUrl" alt="Generated" class="w-full h-auto max-h-full object-cover" />
       </div>
