7375b842ba9a1b931daeb2683a8418acd01a8fa4
Fayche <1751162157@qq.com>
2024-09-12 19:20:50 +0800

    Update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ["./src/**/*.{html,js,jsx,ts,tsx,vue}"],
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx,vue}',
+    '!./src/live2d-static-api/node_modules/**/*' // 排除特定路径
+  ],
   theme: {
     extend: {},
     borderWidth: {
       DEFAULT: '1px',
-      '0': '0',
-      '2': '2px',
-      '3': '3px',
-      '4': '4px',
-      '6': '6px',
-      '8': '8px',
-      '20': '20px',
+      0: '0',
+      2: '2px',
+      3: '3px',
+      4: '4px',
+      6: '6px',
+      8: '8px',
+      20: '20px'
     }
   },
-  plugins: [],
-  
-}
-
+  plugins: []
+};
