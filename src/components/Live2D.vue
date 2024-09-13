9fec74a5573bac1ba9f01ab82e7b1a801c555030
Fayche <1751162157@qq.com>
2024-09-13 13:17:22 +0800

    Update Live2D

--- src/components/Live2D.vue
+++ src/components/Live2D.vue
@@ -3,7 +3,28 @@
 import live2d from 'vue-live2d';
 import { ref } from 'vue';
 
-const model = ref(['Potion-Maker/Pio', 'school-2017-costume-yellow']);
+const skins = [
+  'animal-costume-racoon',
+  'animal-costume',
+  'default-costume',
+  'default',
+  'halloween-costume',
+  'new2015-costume-pajamas',
+  'overalls-costume-white',
+  'overalls-costume',
+  'school-2017-costume-yellow',
+  'school-costume-red',
+  'school-costume',
+  'sorceress-costume',
+  'winter-costume-white'
+];
+
+const getRandomSkin = () => {
+  const randomIndex = Math.floor(Math.random() * skins.length);
+  return skins[randomIndex];
+};
+
+const model = ref(['Potion-Maker/Pio', getRandomSkin()]);
 const apiPath = ref('http://localhost:8080/indexes');
 </script>
 
